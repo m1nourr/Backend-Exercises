45388c02b74df47932cfd34591562badbe559289
Mahmoud Nour <m1nour@MacBook-Air-som-tillhor-Mahmoud.local>
2026-02-11 14:03:52 +0100

    Add tasks routes under /api/v1

--- json-crud-tasks/src/route/index.js
+++ json-crud-tasks/src/route/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import { router as helloRoute } from './hello.js'
+import { router as tasksRoute } from './tasks.js'
 
 export const router = express.Router()
 
 router.use('/', helloRoute)
+router.use('/api/v1', tasksRoute)
