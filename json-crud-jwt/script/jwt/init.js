bac36d60591a957c14d15672bf09912e8938ac2a
Mahmoud Nour <m1nour@MacBook-Air-som-tillhor-Mahmoud.local>
2026-02-17 20:42:46 +0100

    Added node scripts to exercise jwt endpoints

---
+++ json-crud-jwt/script/jwt/init.js
@@ -0,0 +1,11 @@
+const port = process.env.PORT || 3003
+const url = `http://localhost:${port}/api/v1/jwt/init`
+const options = {
+  method: 'POST'
+}
+
+const response = await fetch(url, options)
+const data = await response.json()
+console.log(response.status)
+console.log(data)
+// console.log(response.headers)
