409dea80de834f47047d0710d74ef541735e8402
Mahmoud Nour <m1nour@MacBook-Air-som-tillhor-Mahmoud.local>
2026-02-17 14:26:18 +0100

    Add scripts to test apikey and jwt endpoints

---
+++ json-crud-apikey/script/apikey/try1.js
@@ -0,0 +1,22 @@
+// Get the API key as an argument sent to the script
+if (process.argv.length != 3) {
+  console.error('ERROR. You need to send one arguments like this: <API_KEY>')
+  process.exit(1)
+}
+const API_KEY = process.argv[2]
+
+// Prepare the details for the request
+const url = `http://localhost:${process.env.PORT || 3002}/api/v1/apikey/try1?API_KEY=${API_KEY}`
+const options = {
+  method: 'GET'
+}
+console.log(url)
+
+// Do the request and get the response
+const response = await fetch(url, options)
+const data = await response.json()
+
+// Print the result from the request
+console.log(response.status)
+console.log(data)
+// console.log(response.headers)
---
+++ json-crud-apikey/script/apikey/try_qs.js
@@ -0,0 +1,20 @@
+if (process.argv.length != 3) {
+  console.error('ERROR. You need to send one arguments like this: <API_KEY>')
+  process.exit(1)
+}
+
+const API_KEY = process.argv[2]
+
+const url = `http://localhost:${process.env.PORT || 3002}/api/v1/apikey/try5?API_KEY=${API_KEY}`
+const options = {
+  method: 'GET'
+}
+
+console.log(url)
+
+const response = await fetch(url, options)
+const data = await response.json()
+
+console.log(response.status)
+console.log(data)
+// console.log(response.headers)
