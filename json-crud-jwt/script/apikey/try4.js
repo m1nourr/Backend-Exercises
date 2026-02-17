d909e8558e25b25ddaca19fcd9ddf5ca4766f4d5
Mahmoud Nour <m1nour@MacBook-Air-som-tillhor-Mahmoud.local>
2026-02-17 20:45:39 +0100

    Client scripts to test apikey routes

---
+++ json-crud-jwt/script/apikey/try4.js
@@ -0,0 +1,13 @@
+const url = `http://localhost:${process.env.PORT || 3003}/api/v1/apikey/try4`
+const options = {
+  method: 'GET'
+}
+
+console.log(url)
+
+const response = await fetch(url, options)
+const data = await response.json()
+
+console.log(response.status)
+console.log(data)
+// console.log(response.headers)
