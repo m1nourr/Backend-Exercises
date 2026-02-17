3d6b85315cb23795b8d62e75b0c03c4ea04475ea
Mahmoud Nour <m1nour@MacBook-Air-som-tillhor-Mahmoud.local>
2026-02-17 20:30:03 +0100

    Remove jwt-related files from apikey exercise

--- json-crud-apikey/src/route/index.js
+++ json-crud-apikey/src/route/index.js
@@ -1,10 +1,8 @@
 import express from 'express'
 import { router as apikeyRoute } from './apikeyRoute.js'
-import { router as jwtRoute } from './jwtRoute.js'
 import { router as usersRoute } from './usersRoute.js'
 
 export const router = express.Router()
 
 router.use('/api/v1', apikeyRoute)
-router.use('/api/v1', jwtRoute)
 router.use('/api/v1', usersRoute)
