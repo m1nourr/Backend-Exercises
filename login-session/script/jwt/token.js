f17be946680c15f0ebbbf454e3ac19ea97e3aa6f
Mahmoud Nour <m1nour@MacBook-Air-som-tillhor-Mahmoud.local>
2026-03-10 23:03:10 +0100

    Add helper scripts

---
+++ login-session/script/jwt/token.js
@@ -0,0 +1,20 @@
+if (process.argv.length != 3) {
+  console.error('ERROR. You need to send one argument like this: <jwt token>')
+  process.exit(1)
+}
+
+const port = process.env.PORT || 3004
+const jwtToken = process.argv[2]
+const url = `http://localhost:${port}/api/v1/jwt/token`
+const options = {
+  method: 'GET',
+  headers: {
+    Authorization: jwtToken
+  }
+}
+
+const response = await fetch(url, options)
+const data = await response.json()
+console.log(response.status)
+console.log(data)
+// console.log(response.headers)
