59b1c9dd7f90bb611d44be3d2fef1df2dacda006
Mahmoud Nour <m1nour@MacBook-Air-som-tillhor-Mahmoud.local>
2026-03-10 22:14:52 +0100

    Add database setup

---
+++ login-base/src/config/database.js
@@ -0,0 +1,11 @@
+/**
+ * Configuration file for database connection.
+ */
+export const config = {
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT || 3306),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_SCHEMA
+}
+
