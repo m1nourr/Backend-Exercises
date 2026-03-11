e9545f1858efd35e8dc9cde3b4209b2e1ba63b29
Mahmoud Nour <m1nour@MacBook-Air-som-tillhor-Mahmoud.local>
2026-03-11 02:32:52 +0100

    Wire security routes

--- Lab/src/route/index.js
+++ Lab/src/route/index.js
@@ -1,6 +1,10 @@
 import express from 'express'
 import { router as tasksRoute } from './tasksRoute.js'
+import { router as apiKeyRoute } from './apiKeyRoute.js'
+import { router as jwtRoute } from './jwtRoute.js'
 
 export const router = express.Router()
 
 router.use('/api/v1', tasksRoute)
+router.use('/api/v1', apiKeyRoute)
+router.use('/api/v1', jwtRoute)
